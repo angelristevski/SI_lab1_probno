b40e145ff79fe27327253400ef304e3a9b855910
Angel Ristevski <angel.r98@live.com>
2020-03-24 00:53:51 +0100

    lab points variable added

--- Student.java
+++ Student.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
-
+	List<Integer> labPoints = new LinkedList<Integer>();
 	//TODO constructor
 
 	//TODO seters & getters
